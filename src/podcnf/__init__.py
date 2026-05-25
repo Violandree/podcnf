5d4162f45065acfba225a2724fd350b5ff9c5737
Andrea Violante <violanteandrea15@gmail.com>
2026-05-25 13:52:24 +0000

    Initial setup: gitignore and pyproject.toml

---
+++ src/podcnf/__init__.py
@@ -0,0 +1,9 @@
+# Questo file serve per far capire all'interprete python che il progetto
+# cui siamo dentro non è una semplice cartella ma un pacchetto e che le 
+# sotto cartelle (moduli) devono comunicare tra di loro
+
+# NON SERVE PER VEDERE QUELLO CHE HAI SCRITTO QUA', E' SOLO UNA PROVA
+# print(dir()) # Per vedere variabili di default e quelle istanziate
+# import sys
+# print(type(sys)) # sys è un oggetto modulo
+# print(sys.path) # per vedere se il progetto podcnf è presente nel path
