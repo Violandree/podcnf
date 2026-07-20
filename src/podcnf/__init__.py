c2eeab2e43d391d02e443702d4135d50c3c648a8
Andrea Violante <violanteandrea15@gmail.com>
2026-07-20 12:26:03 +0000

    New pyproject

--- src/podcnf/__init__.py
+++ src/podcnf/__init__.py
@@ -1,2 +1,15 @@
-from .roms import PODcnf
-from .DataManage import LoadData
+# Questo file serve per far capire all'interprete python che il progetto
+# cui siamo dentro non è una semplice cartella ma un pacchetto e che le 
+# sotto cartelle (moduli) devono comunicare tra di loro
+
+# NON SERVE PER VEDERE QUELLO CHE HAI SCRITTO QUA', E' SOLO UNA PROVA
+# print(dir()) # Per vedere variabili di default e quelle istanziate
+# import sys
+# print(type(sys)) # sys è un oggetto modulo
+# print(sys.path) # per vedere se il progetto podcnf è presente nel path
+
+try:
+    from podcnf.roms import PODcnf
+    from podcnf.DataManage import LoadData
+except:
+    None
